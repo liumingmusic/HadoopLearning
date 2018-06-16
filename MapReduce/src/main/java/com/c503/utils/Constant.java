da020b3554f89514d649d3dae01eb455579a1b39
liumingmusic <liuming_music@163.com>
2018-06-16 18:01:32 +0800

    map程序编写

---
+++ MapReduce/src/main/java/com/c503/utils/Constant.java
@@ -0,0 +1,15 @@
+package com.c503.utils;
+
+/**
+ * 描述: 定义常量类
+ *
+ * @Author liumm
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Date 2018-06-15 20:33
+ */
+public class Constant {
+
+    public static final String PATH_URL = "/Users/liuxm/A_study/idea_ws/";
+
+}
