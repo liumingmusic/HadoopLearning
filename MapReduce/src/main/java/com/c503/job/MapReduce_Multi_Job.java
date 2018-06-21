7ce995cb002a9e28cbbaf1ea0e146da6270890f2
liumingmusic <liuming_music@163.com>
2018-06-21 16:12:04 +0800

    maven相关文档

--- MapReduce/src/main/java/com/c503/job/MapReduce_Multi_Job.java
+++ MapReduce/src/main/java/com/c503/job/MapReduce_Multi_Job.java
@@ -1,6 +1,7 @@
 package com.c503.job;
 
-import com.c503.mr.MapReduce_Split;
+import com.c503.mr.MapReduce_Basic;
+import com.c503.mr.MapReduce_Multi;
 import com.c503.utils.Constant;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -10,46 +11,36 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
- * 描述: 参数传递
+ * 描述: 第一个单词计数mr启动端
  *
  * @Author liumm
  * @UpdateRemark: 更新说明
  * @Version: 1.0
  * @Date 2018-06-15 20:25
  */
-public class MapReduce_Parameter_Job {
+public class MapReduce_Multi_Job {
 
-
-    private static String inputPath = Constant.PATH_URL + "mapreduce/inputFile";
-    private static String outputPath = Constant.PATH_URL + "mapreduce/outputFile";
-
-    static {
-        new File(outputPath).delete();
-    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
-
-        //传递参数
-        Configuration conf = new Configuration();
-        conf.set("parameter", "liumm");
+        String inputPath = Constant.PATH_URL + "mapreduce/inputFile";
+        String outputPath = Constant.PATH_URL + "mapreduce/outputFile";
 
         // 创建job
-        Job job = Job.getInstance(conf, "wordcount");
-        job.setJarByClass(MapReduce_Split.class);
-        job.setNumReduceTasks(2);
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration, "wordcount");
+        job.setJarByClass(MapReduce_Basic.class);
 
         // 设置map相关参数
-        job.setMapperClass(MapReduce_Split.MyMapper.class);
+        job.setMapperClass(MapReduce_Multi.MyMapper.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(LongWritable.class);
 
         // 设置reduce相关参数
-        job.setReducerClass(MapReduce_Split.MyReduce.class);
+        job.setReducerClass(MapReduce_Multi.MyReduce.class);
 
         // 设置作业的输入路径
         FileInputFormat.addInputPath(job, new Path(inputPath));
