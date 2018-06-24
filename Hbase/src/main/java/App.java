7333fbdc2fab645b46a8ecbd90832e519799b358
liumingmusic <liuming_music@163.com>
2018-06-24 20:38:55 +0800

    修改图片显示问题

---
+++ Hbase/src/main/java/App.java
@@ -0,0 +1,13 @@
+package PACKAGE_NAME;
+
+/**
+ * 描述: 测试代码
+ *
+ * @Author liumm
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Date 2018-06-24 20:08
+ */
+public class App {
+
+}
---
+++ Hbase/src/test/java/App.java
@@ -0,0 +1,13 @@
+package PACKAGE_NAME;
+
+/**
+ * 描述: 测试程序
+ *
+ * @Author liumm
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Date 2018-06-24 20:10
+ */
+public class App {
+
+}
