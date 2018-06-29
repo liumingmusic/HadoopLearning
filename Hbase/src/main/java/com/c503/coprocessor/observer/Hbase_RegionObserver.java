43de45d011c3c2e11cde6c218a9e24f7c9da6005
liumingmusic <liuming_music@163.com>
2018-06-29 08:58:49 +0800

    协处理器的配置

--- Hbase/src/main/java/com/c503/coprocessor/observer/Hbase_RegionObserver.java
+++ Hbase/src/main/java/com/c503/coprocessor/observer/Hbase_RegionObserver.java
@@ -16,7 +16,22 @@ import org.apache.hadoop.hbase.util.Bytes;
 import java.io.IOException;
 
 /**
- * 描述: 协处理器
+ * 描述: Region协处理器，操作表时，往日志表中写数据。类似数据库中的触发器
+ * 配置说明：
+ * 修改hbase-site.xml，添加如下
+ *      通过hbase.coprocessor.region.classes 配置 RegionObservers 和 Endpoints.
+ *      通过hbase.coprocessor.wal.classes 配置 WALObservers.
+ *      通过hbase.coprocessor.master.classes 配置MasterObservers.
+ *
+ * <property>
+ *     <name>hbase.coprocessor.region.classes</name>
+ *     <value>com.c503.coprocessor.observer.Hbase_RegionObserver</value>
+ * </property>
+ * <property>
+ *     <name>hbase.coprocessor.master.classes</name>
+ *     <value>com.c503.coprocessor.observer.Hbase_MasterObserver</value>
+ * </property>
+ *
  *
  * @Author liumm
  * @UpdateRemark: 更新说明
@@ -28,7 +43,7 @@ public class Hbase_RegionObserver extends BaseRegionObserver {
 
     private Connection connection;
 
-    private final String LOG_TABLE = "logs";
+    private final String LOG_TABLE = "default:logs";
 
     private final String FAMILY = "info";
 
@@ -60,7 +75,7 @@ public class Hbase_RegionObserver extends BaseRegionObserver {
     }
 
     @Override
-    public void stop(CoprocessorEnvironment e) throws IOException {
+    public void stop(CoprocessorEnvironment e) {
         log.info("Hbase_RegionObserver.stop ...");
     }
 
@@ -84,25 +99,34 @@ public class Hbase_RegionObserver extends BaseRegionObserver {
      * @return void
      */
     private void putLog(ObserverContext<RegionCoprocessorEnvironment> e, String p) {
+        //通过RegionCoprocessorEnvironment获取操作表对象
         RegionCoprocessorEnvironment evn = e.getEnvironment();
         TableName tableName = evn.getRegion().getTableDesc().getTableName();
+        //判断当前曹组的分区不是hbase并且表名不是logs(hbase是系统表，logs是操作日志表，防止写入死循环)
         if (!"hbase".equalsIgnoreCase(tableName.getNamespaceAsString()) &&
                 !LOG_TABLE.equalsIgnoreCase(tableName.getNameWithNamespaceInclAsString())) {
+            //获取分区ID
             long regionId = evn.getRegion().getRegionInfo().getRegionId();
+            //打印对应的消息
             if ("P".equalsIgnoreCase(p)) {
                 log.info("Hbase_RegionObserver.putLog:" + regionId + "... postPut");
             } else if ("D".equalsIgnoreCase(p)) {
                 log.info("Hbase_RegionObserver.putLog:" + regionId + "... postDelete");
             }
+            //获取当前写入数据的表名
             String table = tableName.getNameAsString();
+            //获取当前操作的时间
             long millis = System.currentTimeMillis();
+            //组装rowkey
             String rowkey = table + ":" + p + ":" + (-millis);
+            //组装一个put对象
             Put logPut = new Put(Bytes.toBytes(rowkey));
             logPut.addColumn(Bytes.toBytes(FAMILY), Bytes.toBytes(COLUMN_NAME), Bytes.toBytes(table));
             logPut.addColumn(Bytes.toBytes(FAMILY), Bytes.toBytes(COLUMN_TYPE), Bytes.toBytes(p));
             logPut.addColumn(Bytes.toBytes(FAMILY), Bytes.toBytes(COLUMN_TIME), Bytes.toBytes(millis));
             Table logTable = null;
             try {
+                //写入数据到logs表中
                 logTable = connection.getTable(TableName.valueOf(LOG_TABLE));
                 logTable.put(logPut);
             } catch (IOException e1) {
